8b35dc7051b41669570c1c16c104e2d91564845c
samuel <shebabalraj@gmail.com>
2022-06-25 19:59:39 +0530

    1 commit

--- first.py
+++ first.py
@@ -1,10 +1,11 @@
 array = [20,50,30,10,60,80]
-key = 20
+key = int(input("Enter a key number:  "))
 
 def linear_search(list,k):
     for i in range(len(list)):
         if k == list[i]:
-            print("found")
+            print("found at ",i+1)
+            break
 
     else:
         print("not found")
